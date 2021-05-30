6e5ac9f4202261340d7c5b768089d6eb7929ba1d
st3ffn <funk.up.up@gmail.com>
2021-05-30 21:05:21 +0200

    add doc for printer

--- internal/printer/printer.go
+++ internal/printer/printer.go
@@ -17,6 +17,7 @@ type Printer interface {
 
 type DefaultPrinter struct{}
 
+// Print prints the amount of plots left
 func (d DefaultPrinter) Print(info *disk.PlotInfo) {
 	_, _ = fmt.Fprintln(Output, info.PlotsLeft())
 }
@@ -25,6 +26,8 @@ type VerbosePrinter struct {}
 
 const verboseHeader string = "Path\tTotal\tStored\tReserved\tLeft\n"
 
+// Print prints two lines with the following columns:
+// Path, Total Amount of Plots, Amount of Plots Stored, Plots Left
 func (v VerbosePrinter) Print(info *disk.PlotInfo) {
 	w := tabwriter.NewWriter(Output, 0, 0, 1, ' ', tabwriter.TabIndent)
 	_, _ = fmt.Fprint(w, verboseHeader)
